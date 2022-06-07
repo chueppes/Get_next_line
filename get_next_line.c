8edf656ff55675c0252b1acb75a6a0b1a8fa1e3b
acalvo4 <annaclara.tertu@gmail.com>
2022-06-07 14:57:50 -0300

    tentando ler uma linha por vez

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acalvo4 <acalvo4@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 14:56:18 by acalvo4           #+#    #+#             */
-/*   Updated: 2022/06/06 16:31:59 by acalvo4          ###   ########.fr       */
+/*   Updated: 2022/06/07 11:43:17 by acalvo4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,75 @@
 #include <fcntl.h>
 
 
-char *ft_read_line_fd( char *line, int fd);
+//char *ft_read_line_fd( char *line, int fd);
 
 char *get_next_line(int fd)
 {
 	static char *line;
-
-	line = ft_read_line_fd(line, fd);
-
-	return (line);
-}
-
-char *ft_read_line_fd( char *line, int fd){
-	
 	int aux;
 	char *buffer;
+	char *temp;
 
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	// the following line shows me the possible entry errors that may occur
+	// if i don't have a file, if the file is bigger than OPEN_MAX which id the max value of open()
+	// or if I have a negative BUFFER_SIZE
+	// according to the rules, it showld return -1 if an error has occured
+	// but i still don't know how to do it. 
+	if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+		return (NULL);//-1
+	// line = ft_read_line_fd(line, fd);
+	// if everything is ok with the entry values it mallocates my buffer 
+	// with the BUFFER_SIZE + 1 (the null byte) and then i check if the malloc occured fine 
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char *));
 	if (!buffer)
 		return (NULL);
 	aux = 1;
-	while (aux > 0 && !ft_strchr(line, '\n'))
+	// temp = buffer;
+	//checks if line[fd] has \n in it
+	// so, while line doesn't shows me a '\n' i keep on reading the file
+	// if the read command fails it returns the function
+	// else, it puts a line break at the buffer at aux position
+	// after that, it joins the temporary variable which holds the buffer old value
+	// if frees the temporary variable 
+	// the it checks if the line contains a '\n'
+	// i tried on using this comparation first, as a condition to my 'while'
+	// as if if its not a \n i follow the code, but it didn't worked. i dont know why
+	/*while (aux > 0)
 	{
 		aux = read(fd, buffer, BUFFER_SIZE);
 		if (aux < 0)
-			return(NULL);
+			return(NULL);//-1
 		buffer[aux] = '\0';
-		line  = ft_strjoin(line, buffer);
+		temp = line;
+		line  = ft_strjoin(temp, buffer);
+		//free(temp);
+		if (ft_strchr(line, '\n'))
+			break;
+	}*/
+
+	while (aux > 0)
+	{
+		aux = read(fd, buffer, BUFFER_SIZE);
+		if (aux < 0)
+			return (NULL);
+		buffer[aux]
+		temp = line;
+		line = ft_strjoin(temp + fd, buffer);
+		if (ft_strchr(line + fd, '\n'))
+			break ;	
 	}
+	// i'm trying to find out how to read one line.
+	// at this point my code shows me the value if i retur the buffer
 	free(buffer);
+	
+
 	return (line);
 }
 
-/*int main (){
+//char *ft_read_line_fd( char *line, int fd){
+//}
+
+int main (){
 	
 	static char *line;
 	int fd;
@@ -56,14 +92,14 @@ char *ft_read_line_fd( char *line, int fd){
 	line = get_next_line(fd);
 	while (line)
 	{
+		//line = get_next_line(fd);
 		printf("%s", line);
-		free(line);
 	}
 	//free(line);
 	close(fd);
-}*/
+}
 
-int	main(void)
+/*int	main(void)
 {
 	char	*line;
 	int		i;
@@ -91,5 +127,5 @@ int	main(void)
 	close(fd2);
 	close(fd3);
 	return (0);
-}
+}*/
 
